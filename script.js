8344c5322cebd21bf825f2c24825dfb968353b3f
NguyenEvan <55529290+NguyenEvan@users.noreply.github.com>
2021-03-06 15:04:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var fullItemList =document.getElementById("my-list");
+
+if (fullItemList) {
+  fullItemList.addEventListener("click", checkOffItem, false);
+}
+
+
+function checkOffItem(clicked) {
+  if (clicked.target.tagName == "LI") {
+    clicked.target.classList.toggle("all-done");
+
+    }
+}
+
+
